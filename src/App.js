655328fbc8468e0f404564fc5afd56abef3dce95
Ankesh <ankesh3905222@gmail.com>
2021-12-26 01:07:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,27 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/Home.js";
 import About from "./components/about";
 import "./App.css";
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCQmm-9Zt8Myx9k4J1oHqv19-SO3xeMxZs",
+//   authDomain: "ampplex-website.firebaseapp.com",
+//   projectId: "ampplex-website",
+//   storageBucket: "ampplex-website.appspot.com",
+//   messagingSenderId: "805815349209",
+//   appId: "1:805815349209:web:e2af793fa520441fb3b3c8",
+//   measurementId: "G-R8KQHBJS6H",
+// };
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
 const App = () => {
   return (
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,32 @@
-import React, { useState } from "react";
+import React from "react";
 import "./components.css";
+import Lottie from "lottie-react";
 
 export default function Home() {
-  const [boxShadow, setBoxShadow] = useState("0px 0px 20px 0px skyblue");
+  const images = [
+    { url: require("../Ampplex-app-screenshots/1.png") },
+    { url: require("../Ampplex-app-screenshots/2.png") },
+    { url: require("../Ampplex-app-screenshots/3.png") },
+    { url: require("../Ampplex-app-screenshots/4.png") },
+    { url: require("../Ampplex-app-screenshots/5.png") },
+    { url: require("../Ampplex-app-screenshots/6.png") },
+    { url: require("../Ampplex-app-screenshots/7.png") },
+    { url: require("../Ampplex-app-screenshots/8.png") },
+    { url: require("../Ampplex-app-screenshots/9.png") },
+    { url: require("../Ampplex-app-screenshots/10.png") },
+  ];
+
+  function getWindowDimensions() {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+      width,
+      height,
+    };
+  }
+
+  window.addEventListener("scroll", () => {
+    console.log(window.pageYOffset);
+  });
 
   return (
     <>
@@ -20,21 +44,15 @@ export default function Home() {
         <div>
           <h2 className="app">Download Ampplex app</h2>
           <div
+            className="Download-btn"
             style={{
               width: "50%",
               height: "40%",
               backgroundColor: "#fafafa",
               borderRadius: 100,
-              boxShadow: boxShadow,
               margin: "auto",
               marginBottom: 100,
               marginTop: 50,
-            }}
-            onMouseEnter={() => {
-              setBoxShadow("0px 0px 35px 0px skyblue");
-            }}
-            onMouseLeave={() => {
-              setBoxShadow("0px 0px 20px 0px skyblue");
             }}
           >
             <a
@@ -57,6 +75,289 @@ export default function Home() {
             </a>
           </div>
         </div>
+
+        <h1
+          style={{
+            fontSize: 50,
+            fontWeight: "bold",
+          }}
+        >
+          Watch educational content without any distractions.
+        </h1>
+
+        <Lottie animationData={require("../static/lottie/study.json")} />
+
+        <div className="one">
+          <div
+            style={{
+              marginTop: 200,
+              borderRadius: 100,
+            }}
+            className="Image1"
+          >
+            <img
+              className="img1"
+              src={require("../Ampplex-app-screenshots/1.png")}
+              alt="img1"
+              style={{
+                width: 400,
+                height: 700,
+                borderRadius: 20,
+              }}
+              draggable="false"
+            />
+          </div>
+        </div>
+
+        <h1
+          style={{
+            fontSize: 50,
+            fontWeight: "bold",
+            marginTop: 100,
+          }}
+        >
+          Profile
+        </h1>
+        <div className="one">
+          <div
+            style={{
+              marginTop: 150,
+              borderRadius: 100,
+            }}
+            className="Image1"
+          >
+            <img
+              className="img1"
+              src={require("../Ampplex-app-screenshots/2.png")}
+              alt="img1"
+              style={{
+                width: 400,
+                height: 700,
+                borderRadius: 20,
+              }}
+              draggable="false"
+            />
+          </div>
+        </div>
+
+        <h1
+          style={{
+            fontSize: 50,
+            fontWeight: "bold",
+            marginTop: 100,
+          }}
+        >
+          Edit your profile
+        </h1>
+        <div className="one">
+          <div
+            style={{
+              marginTop: 150,
+              borderRadius: 100,
+            }}
+            className="Image1"
+          >
+            <img
+              className="img1"
+              src={require("../Ampplex-app-screenshots/3.png")}
+              alt="img1"
+              style={{
+                width: 400,
+                height: 700,
+                borderRadius: 20,
+              }}
+              draggable="false"
+            />
+          </div>
+        </div>
+
+        <h1
+          style={{
+            fontSize: 50,
+            fontWeight: "bold",
+            marginTop: 100,
+          }}
+        >
+          Search for users
+        </h1>
+        <div className="one">
+          <div
+            style={{
+              marginTop: 150,
+              borderRadius: 100,
+            }}
+            className="Image1"
+          >
+            <img
+              className="img1"
+              src={require("../Ampplex-app-screenshots/4.png")}
+              alt="img1"
+              style={{
+                width: 400,
+                height: 700,
+                borderRadius: 20,
+              }}
+              draggable="false"
+            />
+          </div>
+        </div>
+
+        <h1
+          style={{
+            fontSize: 50,
+            fontWeight: "bold",
+            marginTop: 100,
+          }}
+        >
+          Ampplex Shorts
+        </h1>
+        <div className="one">
+          <div
+            style={{
+              marginTop: 150,
+              borderRadius: 100,
+            }}
+            className="Image1"
+          >
+            <img
+              className="img1"
+              src={require("../Ampplex-app-screenshots/5.png")}
+              alt="img1"
+              style={{
+                width: 400,
+                height: 700,
+                borderRadius: 20,
+              }}
+              draggable="false"
+            />
+          </div>
+        </div>
+
+        <div className="one">
+          <div
+            style={{
+              marginTop: 150,
+              borderRadius: 100,
+            }}
+            className="Image1"
+          >
+            <img
+              className="img1"
+              src={require("../Ampplex-app-screenshots/6.png")}
+              alt="img1"
+              style={{
+                width: 400,
+                height: 700,
+                borderRadius: 20,
+              }}
+              draggable="false"
+            />
+          </div>
+        </div>
+
+        <h1
+          style={{
+            fontSize: 40,
+            fontWeight: "bold",
+            marginTop: 100,
+          }}
+        >
+          Report or block user if you found that the content is inappropriate
+          for childrens
+        </h1>
+        <div className="one">
+          <div
+            style={{
+              marginTop: 150,
+              borderRadius: 100,
+            }}
+            className="Image1"
+          >
+            <img
+              className="img1"
+              src={require("../Ampplex-app-screenshots/7.png")}
+              alt="img1"
+              style={{
+                width: 400,
+                height: 700,
+                borderRadius: 20,
+              }}
+              draggable="false"
+            />
+          </div>
+        </div>
+
+        <h1
+          style={{
+            fontSize: 40,
+            fontWeight: "bold",
+            marginTop: 100,
+          }}
+        >
+          Express your experience
+        </h1>
+        <div className="one">
+          <div
+            style={{
+              marginTop: 150,
+              borderRadius: 100,
+            }}
+            className="Image1"
+          >
+            <img
+              className="img1"
+              src={require("../Ampplex-app-screenshots/8.png")}
+              alt="img1"
+              style={{
+                width: 400,
+                height: 700,
+                borderRadius: 20,
+              }}
+              draggable="false"
+            />
+          </div>
+        </div>
+
+        <h1
+          style={{
+            fontSize: 40,
+            fontWeight: "bold",
+            marginTop: 100,
+          }}
+        >
+          Login to be a part of Ampplex
+        </h1>
+        <div className="one">
+          <div
+            style={{
+              marginTop: 150,
+              borderRadius: 100,
+            }}
+            className="Image1"
+          >
+            <img
+              className="img1"
+              src={require("../Ampplex-app-screenshots/10.png")}
+              alt="img1"
+              style={{
+                width: 400,
+                height: 700,
+                borderRadius: 20,
+              }}
+              draggable="false"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="footer">
+        <p
+          style={{
+            color: "white",
+            textAlign: "center",
+          }}
+        >
+          Copyright © 2021 Ampplex contact us : team.amplex@gmail.com
+        </p>
       </div>
     </>
   );
